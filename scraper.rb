604f611c091264f23aeca6541d5bd2d7bf7e4cb2
Kylie O'Brien-Pratt <Kylie@Kylies-MacBook-Air.local>
2015-10-25 14:09:42 +1100

    save first row of scrabed data

--- scraper.rb
+++ scraper.rb
@@ -9,9 +9,44 @@ agent = Mechanize.new
 # Read in a page
 page = agent.get("http://www.rfs.nsw.gov.au/fire-information/fdr-and-tobans")
 
-name = page.at("table.danger-ratings-table tbody").search("tr").at("td").text
+#name = page.at("table.danger-ratings-table tbody").search("tr").at("td").text
 
-p name
+name = page.at("table.danger-ratings-table tbody").search("tr")[0].search("td")[0].text
+
+today_fire_danger_rating = page.at("table.danger-ratings-table tbody").search("tr")[0].search("td")[1].text
+
+today_total_fire_ban = page.at("table.danger-ratings-table tbody").search("tr")[0].search("td")[2].text
+
+tomorrow_fire_danger_rating = page.at("table.danger-ratings-table tbody").search("tr")[0].search("td")[3].text
+
+tomorrow_total_fire_ban = page.at("table.danger-ratings-table tbody").search("tr")[0].search("td")[4].text
+
+councils_affected = page.at("table.danger-ratings-table tbody").search("tr")[0].search("td")[5].text
+
+record = {
+  name: name,
+  today_fire_danger_rating: today_fire_danger_rating,
+  today_total_fire_ban: today_total_fire_ban,
+  tomorrow_fire_danger_rating: tomorrow_fire_danger_rating,
+  tomorrow_total_fire_ban: tomorrow_total_fire_ban,
+  councils_affected: councils_affected
+}
+
+ScraperWiki.save_sqlite([:name], record)
+
+# p name
+
+# p today_fire_danger_rating
+
+# p tomorrow_fire_danger_rating
+
+# p councils_affected			
+
+# p tomorrow_total_fire_ban			
+
+# p today_total_fire_ban		
+
+p record		
 
 #
 # # Write out to the sqlite database using scraperwiki library
